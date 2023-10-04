0e8ea29a989bcd4e162b274ed6313d7ef72723ed
natyee04 <natalienat.05@gmail.com>
2023-10-04 15:26:39 -0400

    Created base for getRepresentation method in HuntingtonHillMethod.java in HuntingtonHillMethod branch

--- src/main/java/sde/virginia/edu/hw3/HuntingtonHillMethod.java
+++ src/main/java/sde/virginia/edu/hw3/HuntingtonHillMethod.java
@@ -5,5 +5,11 @@ import java.util.List;
 import java.util.Map;
 
 public class HuntingtonHillMethod implements ApportionmentMethod{
+    @Override
+    public Representation getRepresentation(List<State> states, int targetRepresentatives) {
+        Map<State, Integer> representation = new HashMap<>();
+        //implement HuntingtonHill algo here
+        return new Representation(representation);
+    }
 
 }
