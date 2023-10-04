e03788d0436f37cea4e979f26958e85a9391f8f7
natyee04 <natalienat.05@gmail.com>
2023-10-04 15:25:52 -0400

    Created HuntingtonHillMethod.java file in HuntingtonHillMethod branch

--- src/main/java/sde/virginia/edu/hw3/HuntingtonHillMethod.java
+++ src/main/java/sde/virginia/edu/hw3/HuntingtonHillMethod.java
@@ -5,29 +5,5 @@ import java.util.List;
 import java.util.Map;
 
 public class HuntingtonHillMethod implements ApportionmentMethod{
-    @Override
-    public Representation getRepresentation(List<State> states, int targetRepresentatives) {
-        validateInputs(states, targetRepresentatives);
-        Map<State, Integer> representation = new HashMap<>();
 
-        for (State state: states){
-            representation.put(state, 1);
-        }
-
-        int remainRepresentatives = targetRepresentatives - states.size();
-        while(remainRepresentatives>0){
-            double maxPriorityScore = 0.0;
-            State maxPriorityState = null;
-            for(State state:states){
-                double priorityScore = state.population()/(Math.sqrt((representation.get(state))*(representation.get(state) + 1)));
-                if(priorityScore>maxPriorityScore){
-                    maxPriorityState = state;
-                    maxPriorityScore = priorityScore;
-                }
-            }
-            representation.put(maxPriorityState, 1 + representation.get(maxPriorityState));
-            remainRepresentatives -= 1;
-        }
-        return new Representation(representation);
-    }
 }
