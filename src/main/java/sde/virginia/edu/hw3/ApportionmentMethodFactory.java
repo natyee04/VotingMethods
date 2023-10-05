0c958051cbac80eeee9a50a98cdc4b1c87a442ba
Eva-Fong <evafong26750@gmail.com>
2023-10-05 19:41:27 -0400

    filled out the factory class for the apportionment methods.

--- src/main/java/sde/virginia/edu/hw3/ApportionmentMethodFactory.java
+++ src/main/java/sde/virginia/edu/hw3/ApportionmentMethodFactory.java
@@ -1,4 +1,21 @@
 package sde.virginia.edu.hw3;
 
 public class ApportionmentMethodFactory {
+    public ApportionmentMethod getDefaultMethod() {
+        return new HuntingtonHillMethod();
+    }
+
+    public ApportionmentMethod getMethod(String method) {
+        if (method.equals("adams")) {
+            return new AdamsMethod();
+        }
+        if (method.equals("jefferson")) {
+            return new JeffersonMethod();
+        }
+        if (method.equals("huntington")) {
+            return new HuntingtonHillMethod();
+        }
+
+        throw new IllegalArgumentException("the given input is invalid. valid inputs: adams, jefferson, or huntington");
+    }
 }
