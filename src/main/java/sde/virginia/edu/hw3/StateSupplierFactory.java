cffa9dcfa95b8c203fb915d49371590187b94dd4
Eva-Fong <evafong26750@gmail.com>
2023-10-05 19:33:53 -0400

    filled out the factory class for getting state supplier.

--- src/main/java/sde/virginia/edu/hw3/StateSupplierFactory.java
+++ src/main/java/sde/virginia/edu/hw3/StateSupplierFactory.java
@@ -1,4 +1,13 @@
 package sde.virginia.edu.hw3;
 
 public class StateSupplierFactory {
+    public StateSupplier getStateSupplier(String filename) {
+        if (filename.toLowerCase().endsWith("csv")) {
+            return new CSVStateReader(filename);
+        }
+        if (filename.toLowerCase().endsWith("xlsx") || filename.toLowerCase().endsWith("xls")) {
+            return new SpreadsheetStateReader(filename);
+        }
+        throw new UnsupportedFileFormatException(filename);
+    }
 }
